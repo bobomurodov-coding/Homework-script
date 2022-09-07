e9a7593d6a82714b78cc9b322a0210fb9be1f152
Bobomurodov Umar <109015553+bobomurodov-coding@users.noreply.github.com>
2022-09-07 17:11:34 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,64 @@
+//            First
+// let a = 10;
+// for(i = 0; i <= a; i++){
+//  console.log(a)
+// }
+
+// Second
+// let a = 5, b = 10;
+// for(j = a; j <= b; j++){
+//     console.log(j);
+// }
+
+// Third
+// let a = -1;
+// for(i = 10; a < i; i--){
+//     console.log(`${i}`);
+// }
+
+// Fourth
+// for(j = 0; j <= 10; j++){
+//     console.log(`${j}kg + olma = ${j*2000}so'm`);
+// }
+
+// Fifth
+// let a = 50000;
+// for(i = 1; i <= 10; i++){
+//     console.log((`${i/10}kanfet = ${i/10*a}so'm`));
+// }
+
+// Sixth
+// let a = 50000;
+// for(i = 12; i <= 20; i++){
+//     console.log((`${i/10}kanfet = ${i/10*a}so'm`));
+// }
+
+// Seventh
+// let a = 20;
+// for(i = 2; i<20; i++){
+//     a +=i
+//     console.log(a);
+// }
+
+// Eventh
+// let a = 10;
+// for(i = 2; i<20; i++){
+//     a *=i
+//     console.log(a);
+// }
+
+//  Fiften 
+//  let a = 0;
+//  for(i = 2; i<=3; i++){
+//      a +=i**2
+    
+//  }console.log(a);
+
+// // Tenth
+// let a = 0;
+// for(let n = 1; n <=10; n++){
+//     a=1/2 
+// }console.log(a);
+
+
+
